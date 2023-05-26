f6845e977b44dec238e210a07f0a1a48b6ea041e
JoakimGullander <91761274+JoakimGullander@users.noreply.github.com>
2023-05-26 23:00:48 +0200

    Add project files.

---
+++ Bibliotek övning/Data.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Bibliotek_övning
+{
+    public class Data
+    {
+        public string userfilename = "usernames.txt";
+        public string passfilename = "passwords.txt";
+
+        public string[] getusernameLines() 
+        {
+            
+            string[] usernamefilelines = File.ReadAllLines(userfilename);
+            
+            return usernamefilelines;
+        }
+        public string[] getPasswordLines()
+        {
+
+            string[] passwordfilelines = File.ReadAllLines(passfilename);
+
+            return passwordfilelines;
+        }
+
+    }
+}
